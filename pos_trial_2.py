fd89c874310445fb8bfaaca986c1a2a514609fb6
Krupa Shah <shahkrupa25@gmail.com>
2020-01-02 12:52:34 +0530

    Set debug to True

--- pos_trial_2.py
+++ pos_trial_2.py
@@ -55,4 +55,4 @@ def adj():
 
 
 if __name__ == '__main__':
-    app.run()                                                                                                                                       
+    app.run(debug=True)                                                                                                                                       
